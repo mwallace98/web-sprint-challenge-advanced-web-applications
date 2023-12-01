c96af6a1f8f0a8b85f550a65f89d62e1e7b03eb2
mwallace98 <mattwallace140@gmail.com>
2023-11-30 19:18:15 -0500

    logout button completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -26,6 +26,8 @@ export default function App() {
 
   const logout = () => {
     localStorage.removeItem('token')
+    setMessage('Goodbye!')
+    navigate('/')
     // ✨ implement
     // If a token is in local storage it should be removed,
     // and a message saying "Goodbye!" should be set in its proper state.
